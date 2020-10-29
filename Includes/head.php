45242ebfba0df8e793c06c215639f2dd2ca2b04a
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2020-10-29 16:11:00 +0530

    29/10/2020

---
+++ Includes/head.php
@@ -0,0 +1,15 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="xmlrpc.php">
+	<title>Travel</title>
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/flaticon.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/font-linearicons.css" type="text/css" media="all">
+	<link rel="stylesheet" href="style.css" type="text/css" media="all">
+	<link rel="stylesheet" href="assets/css/travel-setting.css" type="text/css" media="all">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+</head>
---
+++ Includes/navbar.php
@@ -0,0 +1,158 @@
+<header id="masthead" class="site-header sticky_header affix-top">
+		<div class="header_top_bar">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<aside id="text-15" class="widget_text">
+							<div class="textwidget">
+								<ul class="top_bar_info clearfix">
+									<li><i class="fa fa-clock-o"></i> Mon-Sat 8.00 - 6.00. Sunday CLOSED</li>
+								</ul>
+							</div>
+						</aside>
+					</div>
+					<div class="col-sm-8 topbar-right">
+						<aside id="text-7" class="widget widget_text">
+							<div class="textwidget">
+								<ul class="top_bar_info clearfix">
+									<li><i class="fa fa-phone"></i> 0123456789</li>
+									<li class="hidden-info">
+										<i class="fa fa-map-marker"></i>  
+										Kalyani Nagar,Pune,India
+									</li>
+								</ul>
+							</div>
+						</aside>
+						<aside id="travel_login_register_from-2" class="widget widget_login_form">
+							<span data-toggle="modal" data-target="#modalLRForm"class="show_from login"><i class="fa fa-user"></i>Login</span>
+
+							<div class="form_popup from_login" tabindex="-1">
+								<div class="inner-form">
+									<div class="closeicon"></div>
+									<h3>Login</h3>
+									<form name="loginform" id="loginform" action="#" method="post">
+										<p class="login-username">
+											<label for="user_login">Username or Email Address</label>
+											<input type="text" name="log" id="user_login" class="input" value="" size="20">
+										</p>
+										<p class="login-password">
+											<label for="user_pass">Password</label>
+											<input type="password" name="pwd" id="user_pass" class="input" value="" size="20">
+										</p>
+										<p class="login-remember">
+											<label><input name="rememberme" type="checkbox" id="rememberme" value="forever"> Remember Me</label>
+										</p>
+										<p class="login-submit">
+											<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary" value="Log In">
+											<input type="hidden" name="redirect_to" value="">
+										</p>
+									</form>
+									<a href="#" title="Lost your password?" class="lost-pass">Lost your password?</a>
+								</div>
+							</div>
+							<span class="register_btn"> <a href="AgentRegister.php">
+                             Register</a></span>
+							
+							<div class="background-overlay"></div>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navigation-menu">
+			<div class="container" style="padding: 0; margin: 0;  ">
+				<div class="menu-mobile-effect navbar-toggle button-collapse" data-activates="mobile-demo">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</div>
+				<div class="width-logo sm-logo">
+					<a href="" title="Travel" rel="home">
+						<img src="images/logo.png" alt="Logo" width="474" height="130" class="logo_transparent_static">
+						<img src="images/logo_sticky.png" alt="Sticky logo" width="474" height="130" class="logo_sticky">
+					</a>
+				</div>
+				<nav class="width-navigation"style="max-width: 100%;margin: 0;padding: 0">
+
+					<ul class="nav navbar-nav menu-main-menu side-nav" id="mobile-demo">
+						<li class="current-menu-ancestor current-menu-parent">
+							<a style="font-size:16px;color:#ffb300 "href="index.html">Home</a>
+						</li>
+						<li class="menu-item-has-children">
+							<a style="font-size:16px;color:#ffb300 "href="tours.html">International Destination</a>
+							<ul class="sub-menu">				
+									
+										<li><a href="tours-list.html">Unseen Thailand</a></li>
+										<li><a href="tours-2-cols.html">Thailand</a></li>
+										<li><a href="tours.html">South Korea</a></li>
+										<li><a href="tours-3-cols.html">Vietnam</a></li>
+										<li><a href="tours-4-cols.html">Kambodia</a></li>
+										<li><a href="tours-4-cols.html">Isreal</a></li>
+										<li><a href="tours-4-cols.html">Taiwan</a></li>
+										<li><a href="tours-4-cols.html">Scandinavian Countries</a></li>
+										<li><a href="tours-4-cols.html">Philippines</a></li>
+										<li><a href="tours-4-cols.html">French Islands Like Reunion Island</a></li>
+										<li><a href="tours-4-cols.html">Madagascar</a></li>
+										<li><a href="tours-4-cols.html">Zanzibar</a></li>
+										<li><a href="tours-4-cols.html">Spanish Islands Like Formentera</a></li>
+										<li><a href="tours-4-cols.html">Costa Brava</a></li>
+										<li><a href="tours-4-cols.html"> Ibiza</a></li>
+										<li><a href="tours-4-cols.html"> Gran Canaria</a></li>
+										<li><a href="tours-4-cols.html"> Minorca And Some More</a></li>
+									</ul>
+						</li>
+
+						<li class="menu-item-has-children">
+							<a style="font-size:16px;color:#ffb300 "href="tours.html">North East</a>
+							<ul class="sub-menu">				
+									
+										<li><a href="tours-list.html">Arunachal Pradesh</a></li>
+										<li><a href="tours-2-cols.html">Nagaland</a></li>
+										<li><a href="tours.html">Mizoram</a></li>
+										<li><a href="tours-3-cols.html">Manipur</a></li>
+										<li><a href="tours-4-cols.html">Tripura</a></li>
+										<li><a href="tours-4-cols.html">Assam </a></li>
+									
+									</ul>
+						</li>											
+
+						<li><a style="font-size:16px;color:#ffb300 "href="destinations.html">Honeymoon Special</a>
+							<ul class="sub-menu">
+						<li><a href="single-tour.html">Domestic</a>
+								<ul class="sub-menu">
+										<li><a href="tours-list.html">Feel The Love Of South</a></li>
+
+										<li><a href="tours-2-cols.html">Romantic Himachal</a></li>
+										<li><a href="tours.html">Romantic Kerala</a></li>
+										</ul>
+								</li>
+								<li><a href="#">International</a>
+									<ul class="sub-menu" style="overflow-y: scroll; height: 200px">
+
+										<li><a href="tours-list.html">Romantic Bali</a></li>
+										<li><a href="tours-2-cols.html">Romantic Europe</a></li>
+										<li><a href="tours.html">Romantic Koh Samui Island</a></li>
+										<li><a href="tours-3-cols.html">Romantic Krabi Island</a></li>
+										<li><a href="tours-4-cols.html">Romantic Maldives</a></li>
+										<li><a href="tours-4-cols.html">Romantic Mauritius</a></li>
+										<li><a href="tours-4-cols.html">Romantic Seychelles</a></li>
+										<li><a href="tours-4-cols.html">Romantic  Phuket Island</a></li>
+										<li><a href="tours-4-cols.html">Romantic  Australia and new Zealand</a></li>
+									</ul>
+							</li>
+						</ul>
+					</li>
+			
+
+						<li><a style="font-size:16px;color:#ffb300 "href="blog.html">Offers</a></li>
+
+					      <li>
+							<a style="font-size:16px;color:#ffb300 "href="blog.html">Group Holidays</a>
+						</li>
+						
+						<li><a style="font-size:16px;color:#ffb300 "href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
