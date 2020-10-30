121795ea6ae313df7e6cbda4b95bd46106fcafe8
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2020-10-30 15:19:18 +0530

    30/10/2020

--- Arunachal.php
+++ Arunachal.php
@@ -14,33 +14,45 @@
 						
 					</ul>
 				</div>
-				<h1 class="heading_primary">Romantic Europe</h1></div>
+				<h1 class="heading_primary">Arunachal</h1></div>
 		</div>
 		<section class="content-area">
 			<div class="container">
 				<div class="row">
 					<div class="site-main col-sm-9 alignright">
+
 						<ul class="tours products wrapper-tours-slider">
-							
 							<li class="item-tour col-md-6 col-sm-6 product">
+							 <div class="carousel-inner">
 								<div class="item_border item-product">
-									<div class="post_images">
-										<a href="single-tour.html">
-											<img width="430" height="305" src="images/Arunachalimg/Bhalukpong.jpg" alt="World’s hottest destinations for vegans" title="World’s hottest destinations for vegans ">
-										</a>
-										<div class="group-icon">
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" class="frist" data-original-title="Escorted Tour"><i class="flaticon-airplane-4"></i></a>
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rail Tour"><i class="flaticon-cart-1"></i></a>
-										</div>
-									</div>
+									<div style="border:1px soild #000;">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+
+        <img src="images/Arunachalimg/Bhalukpong.jpg" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="images/Arunachalimg/NamdaphaNationalPark.jpg" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="images/Arunachalimg/KoloriangValley.jpg" style="width:100%;">
+      </div>
+    </div>
+</div>
+  </div>
+
 									<div class="wrapper_content">
 										<div class="post_title"><h4>
 											<a href="single-tour.html" rel="bookmark">Bhalukpong</a>
 										</h4></div>
 										<span class="post_date">Coming Soon</span>
 										<marquee class="ubuntu-l" style="color:#ff7a59;font-size:18px;font-weight:bold;">Room Decoration on 1st Night, Kesar /Badam Milk every Night In The Coorg Resort, Candle Light Dinner, Chocolates,Cookies, Honeymoon Special Cake</marquee>
-
-										
 									</div>
 									<div class="Book Now"style="background-color: #ffb300 ">
 										<div class="item_rating">
@@ -54,82 +66,34 @@
 									</div>
 								</div>
 							</li>
-							<li class="item-tour col-md-6 col-sm-6 product">
-								<div class="item_border item-product">
-									<div class="post_images">
-										<a href="single-tour.html">
-											<img width="430" height="305" src="images/Arunachalimg/DibangRiverValley.jpg" alt="World’s hottest destinations for vegans" title="World’s hottest destinations for vegans ">
-										</a>
-										<div class="group-icon">
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" class="frist" data-original-title="Escorted Tour"><i class="flaticon-airplane-4"></i></a>
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rail Tour"><i class="flaticon-cart-1"></i></a>
-										</div>
-									</div>
-									<div class="wrapper_content">
-										<div class="post_title"><h4>
-											<a href="single-tour.html" rel="bookmark">Dibang River Valley</a>
-										</h4></div>
-										<span class="post_date">Coming Soon</span>
-										<marquee class="ubuntu-l" style="color:#ff7a59;font-size:18px;font-weight:bold;">Room Decoration on 1st Night, Kesar /Badam Milk every Night In The Coorg Resort, Candle Light Dinner, Chocolates,Cookies, Honeymoon Special Cake</marquee>
 
-										
-									</div>
-									<div class="Book Now"style="background-color: #ffb300 ">
-										<div class="item_rating">
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star-o"></i>
-										</div>
-										<a rel="nofollow" href="single-tour.html" class="button product_type_tour_phys add_to_cart_button">Book Now</a>
-									</div>
-								</div>
-							</li>
-							<li class="item-tour col-md-6 col-sm-6 product">
-								<div class="item_border item-product">
-									<div class="post_images">
-										<a href="single-tour.html">
-											<img width="430" height="305" src="images/Arunachalimg/KoloriangValley.jpg" alt="World’s hottest destinations for vegans" title="World’s hottest destinations for vegans ">
-										</a>
-										<div class="group-icon">
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" class="frist" data-original-title="Escorted Tour"><i class="flaticon-airplane-4"></i></a>
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rail Tour"><i class="flaticon-cart-1"></i></a>
-										</div>
-									</div>
-									<div class="wrapper_content">
-										<div class="post_title"><h4>
-											<a href="single-tour.html" rel="bookmark">Koloriang Valley</a>
-										</h4></div>
-										<span class="post_date">Coming Soon</span>
-										<marquee class="ubuntu-l" style="color:#ff7a59;font-size:18px;font-weight:bold;">Room Decoration on 1st Night, Kesar /Badam Milk every Night In The Coorg Resort, Candle Light Dinner, Chocolates,Cookies, Honeymoon Special Cake</marquee>
 
-										
-									</div>
-									<div class="Book Now"style="background-color: #ffb300 ">
-										<div class="item_rating">
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star-o"></i>
-										</div>
-										<a rel="nofollow" href="single-tour.html" class="button product_type_tour_phys add_to_cart_button">Book Now</a>
-									</div>
-								</div>
-							</li>
-							
 							<li class="item-tour col-md-6 col-sm-6 product">
 								<div class="item_border item-product">
-									<div class="post_images">
-										<a href="single-tour.html">
-											<img width="430" height="305" src="images/Arunachalimg/NamdaphaNationalPark.jpg" alt="World’s hottest destinations for vegans" title="World’s hottest destinations for vegans ">
-										</a>
-										<div class="group-icon">
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" class="frist" data-original-title="Escorted Tour"><i class="flaticon-airplane-4"></i></a>
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rail Tour"><i class="flaticon-cart-1"></i></a>
-										</div>
-									</div>
+									<div style="border:1px soild #000;">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+
+        <img src="images/Arunachalimg/DibangRiverValley.jpg" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="images/Arunachalimg/Manmao.jpg" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="images/Arunachalimg/YingkiongValley.jpg" style="width:100%;">
+      </div>
+    </div>
+</div>
+  </div>
+							<li class="item-tour col-md-6 col-sm-6 product">
+								<div class="item_border item-product">
+									
 									<div class="wrapper_content">
 										<div class="post_title"><h4>
 											<a href="single-tour.html" rel="bookmark">Namdapha  National Park</a>
@@ -151,38 +115,7 @@
 									</div>
 								</div>
 							</li>
-							<li class="item-tour col-md-6 col-sm-6 product">
-								<div class="item_border item-product">
-									<div class="post_images">
-										<a href="single-tour.html">
-											<img width="430" height="305" src="images/Arunachalimg/YingkiongValley.jpg" alt="World’s hottest destinations for vegans" title="World’s hottest destinations for vegans ">
-										</a>
-										<div class="group-icon">
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" class="frist" data-original-title="Escorted Tour"><i class="flaticon-airplane-4"></i></a>
-											<a href="tours.html" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rail Tour"><i class="flaticon-cart-1"></i></a>
-										</div>
-									</div>
-									<div class="wrapper_content">
-										<div class="post_title"><h4>
-											<a href="single-tour.html" rel="bookmark">Yingkiong Valley</a>
-										</h4></div>
-										<span class="post_date">Coming Soon</span>
-										<marquee class="ubuntu-l" style="color:#ff7a59;font-size:18px;font-weight:bold;">Room Decoration on 1st Night, Kesar /Badam Milk every Night In The Coorg Resort, Candle Light Dinner, Chocolates,Cookies, Honeymoon Special Cake</marquee>
-
-										
-									</div>
-									<div class="Book Now"style="background-color: #ffb300 ">
-										<div class="item_rating">
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star"></i>
-											<i class="fa fa-star-o"></i>
-										</div>
-										<a rel="nofollow" href="single-tour.html" class="button product_type_tour_phys add_to_cart_button">Book Now</a>
-									</div>
-								</div>
-							</li>
+							
 						</ul>
 						<div class="navigation paging-navigation" role="navigation">
 							<ul class="page-numbers">
@@ -190,6 +123,7 @@
 								<li><a class="page-numbers" href="#">2</a></li>
 								<li><a class="next page-numbers" href="#"><i class="fa fa-long-arrow-right"></i></a>
 								</li>
+
 							</ul>
 						</div>
 					</div>
@@ -219,51 +153,7 @@
 </div>
   </div>
 </div>
-
-						<!-- <div class="search_tour">
-							<div class="form-block block-after-indent">
-								<h3 class="form-block_title">Search Tour</h3>
-								<div class="form-block__description">Find your dream tour today!</div>
-								<form method="get" action="#">
-									<input type="hidden" name="tour_search" value="1">
-									<input type="text" placeholder="Search Tour" value="" name="name_tour">
-									<select name="tourtax[tour_phys]">
-										<option value="0">Tour Type</option>
-										<option value="escorted-tour">Escorted Tour</option>
-										<option value="rail-tour">Rail Tour</option>
-										<option value="river-cruise">River Cruise</option>
-										<option value="tour-cruise">Tour &amp; Cruise</option>
-										<option value="wildlife">Wildlife</option>
-									</select>
-									<select name="tourtax[pa_destination]">
-										<option value="0">Destination</option>
-										<option value="china">Brazil</option>
-										<option value="canada">Canada</option>
-										<option value="cuba">Cuba</option>
-										<option value="italy">Italy</option>
-										<option value="philippines">Philippines</option>
-										<option value="usa">USA</option>
-									</select>
-									<select name="tourtax[pa_month]">
-										<option value="0">Month</option>
-										<option value="january">January</option>
-										<option value="february">February</option>
-										<option value="march">March</option>
-										<option value="april">April</option>
-										<option value="may">May</option>
-										<option value="june">June</option>
-										<option value="july">July</option>
-										<option value="august">August</option>
-										<option value="september">September</option>
-										<option value="october">October</option>
-										<option value="november">November</option>
-										<option value="december">December</option>
-									</select>
-									<button type="submit">Find Tours</button>
-								</form>
-							</div>
-						</div> -->
-						<aside class="widget widget_travel_tour">
+							<aside class="widget widget_travel_tour">
 							<div class="wrapper-special-tours">
 								<div class="inner-special-tours">
 									<a href="single-tour.html">
