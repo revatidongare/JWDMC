848495a10c62e150fad667b0fec66c22402f4798
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2020-11-03 18:17:44 +0530

    3/11/2020
    Language

--- Corporatelogin.php
+++ Corporatelogin.php
@@ -48,7 +48,11 @@
                         <div class="p-t-20">
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                             <button class="btn btn--radius btn--green"type="submit">Login</button>
+=======
+                            <button class="btn btn--radius btn--green" type="submit">Login</button>
+>>>>>>> Stashed changes
 =======
                             <button class="btn btn--radius btn--green" type="submit">Login</button>
 >>>>>>> Stashed changes
