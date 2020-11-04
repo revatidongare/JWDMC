effbc71475e6fef0d27013a65ea3e452ae9df826
pallavitambe1234 <56679787+pallavitambe1234@users.noreply.github.com>
2020-11-04 17:25:33 +0530

    4/11/2020
    Itineries

--- Corporatelogin.php
+++ Corporatelogin.php
@@ -50,7 +50,11 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                             <button class="btn btn--radius btn--green"type="submit">Login</button>
+=======
+                            <button class="btn btn--radius btn--green" type="submit">Login</button>
+>>>>>>> Stashed changes
 =======
                             <button class="btn btn--radius btn--green" type="submit">Login</button>
 >>>>>>> Stashed changes
