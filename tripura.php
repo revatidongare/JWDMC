579a55a435138a1b9aae5a5579c1e91d2d12e43a
vaishnavipatange20 <71210031+vaishnavipatange20@users.noreply.github.com>
2021-01-19 17:50:50 +0530

    1/19/2021
    website updated with product description and made a web page for tripura culture heritage

---
+++ tripura.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<?php include 'includes/head.php';?>
+<body class="transparent_home_page">
+	<div class="wrapper-container">
+		<?php include'includes/header.php';?>
+		<div class="site wrapper-content">
+				<div class="site wrapper-content">
+		<div class="top_site_main" style="background-image:url(images/banner/top-heading.jpg);">
+			<div class="banner-wrapper container article_heading">
+				<div class="breadcrumbs-wrapper">
+					<ul class="phys-breadcrumb">
+						<li><a href="index.html" class="home">Home</a></li>
+						<li>North East</li>
+					</ul>
+				</div>
+				<h1 class="heading_primary">Tripura</h1></div>
+		</div>
+		<section class="content-area">
+			<div class="container">
+				<div class="row">
+					<div class="site-main col-sm-9 alignright">
+						<ul class="tours products wrapper-tours-slider">
+							<li class="item-tour col-md-6 col-sm-6 product">
+								<div class="item_border item-product">
+									<div class="post_images">
+										<a href="Tripura1.php">
+											
+											<img width="430" height="305" src="images/Tripura/430x305/Cultureheritage.jpg" alt="Cultural Heritage" title="Cultural Heritage">
+										</a>
+										
+									</div>
+									<div class="wrapper_content">
+										<div class="post_title"><h4>
+											<a href="Tripura1.php" rel="bookmark">Cultural Heritage</a>
+										</h4></div>
+										<!-- <span class="post_date">7 DAYS 6 NIGHTS</span> -->
+										<div class="description">
+											<p>‘Cultural Relativism' and ‘Cultural Mosaic' dominate scholarly discourse all around.The undulating hillscape of the state-sixty percent of the Tripura’s territory </p>
+										</div>
+									</div>
+									<div class="read_more">
+										<div class="item_rating">
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star"></i>
+											<i class="fa fa-star-o"></i>
+										</div>
+										<a rel="nofollow" href="Tripura1.php" class="button product_type_tour_phys add_to_cart_button">Read more</a>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div class="widget-area align-left col-sm-3">
+						<div class="search_tour">
+							<div class="form-block block-after-indent">
+								<h3 class="form-block_title">Search Tour</h3>
+								<div class="form-block__description">Find your dream tour today!</div>
+								<form method="get" action="#">
+									<input type="hidden" name="tour_search" value="1">
+									<input type="text" placeholder="Search Tour" value="" name="name_tour">
+									<select name="tourtax[tour_phys]">
+										<option value="0">Tour Type</option>
+										<option value="escorted-tour">Escorted Tour</option>
+										<option value="rail-tour">Rail Tour</option>
+										<option value="river-cruise">River Cruise</option>
+										<option value="tour-cruise">Tour &amp; Cruise</option>
+										<option value="wildlife">Wildlife</option>
+									</select>
+									<select name="tourtax[pa_destination]">
+										<option value="0">Destination</option>
+										<option value="china">Brazil</option>
+										<option value="canada">Canada</option>
+										<option value="cuba">Cuba</option>
+										<option value="italy">Italy</option>
+										<option value="philippines">Philippines</option>
+										<option value="usa">USA</option>
+									</select>
+									<select name="tourtax[pa_month]">
+										<option value="0">Month</option>
+										<option value="january">January</option>
+										<option value="february">February</option>
+										<option value="march">March</option>
+										<option value="april">April</option>
+										<option value="may">May</option>
+										<option value="june">June</option>
+										<option value="july">July</option>
+										<option value="august">August</option>
+										<option value="september">September</option>
+										<option value="october">October</option>
+										<option value="november">November</option>
+										<option value="december">December</option>
+									</select>
+									<button type="submit">Find Tours</button>
+								</form>
+							</div>
+						</div>
+						<aside class="widget widget_travel_tour">
+							<div class="wrapper-special-tours">
+								<div class="inner-special-tours">
+									<a href="Tripura1.php">
+										<img width="430" height="305" src="images/Tripura/430x305/Cultureheritage.jpg" alt="Culture Heritage" title="Culture Heritage"></a>
+									<div class="item_rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star-o"></i>
+									</div>
+									<div class="post_title"><h3>
+										<a href="Tripura1.php" rel="bookmark">Culture Heritage</a>
+									</h3></div>
+									<div class="item_price">
+										>
+									</div>
+								</div>
+								<div class="inner-special-tours">
+									<a href="Australia2.php">
+										
+										<img width="430" height="305" src="images/Tripura/430x305/simply.jpg" alt="Culture Heritage" title="Culture Heritage"></a>
+									<div class="item_rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star-o"></i>
+										<i class="fa fa-star-o"></i>
+									</div>
+									<div class="post_title"><h3>
+										<a href="Australia2.php" rel="bookmark">AUSTRALIAN MEMORIES</a>
+									</h3></div>
+									<div class="item_price">
+										
+									</div>
+								</div>
+								<div class="inner-special-tours">
+									<a href="Australia3.php">
+										<img width="430" height="305" src="images/tour/430x305/simply.jpg" alt="WESTERN TRIANGLE" title="WESTERN TRIANGLE">
+									</a>
+									<div class="item_rating">
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star"></i>
+										<i class="fa fa-star-o"></i>
+										<i class="fa fa-star-o"></i>
+										<i class="fa fa-star-o"></i>
+									</div>
+									<div class="post_title"><h3>
+										<a href="Australia3.php" rel="bookmark">SIMPLY AUSTRALIA</a>
+									</h3></div>
+									<div class="item_price">
+										
+									</div>
+								</div>
+							</div>
+						</aside>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<?php include'includes/footer.php';?>
+	
+	</div>
+				<?php include'includes/script.php';?>
+			</body>
+			</html>
---
+++ tripura1.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<?php include'includes/head.php';?>
+<body class="single-product travel_tour-page travel_tour">
+<div class="wrapper-container">
+	<?php include'includes/header.php';?>
+	<div class="site wrapper-content">
+		<div class="top_site_main" style="background-image:url(images/banner/top-heading.jpg);">
+			<div class="banner-wrapper container article_heading">
+				<div class="breadcrumbs-wrapper">
+					<ul class="phys-breadcrumb">
+						<li><a href="index.php" class="home">Home</a></li>
+						
+					</ul>
+				</div>
+				<h2 class="heading_primary">North East</h2></div>
+		</div>
+		<section class="content-area single-woo-tour">
+			<div class="container">
+				<div class="tb_single_tour product">
+					<div class="top_content_single row">
+						<div class="images images_single_left">
+							<div class="title-single">
+								<div class="title">
+									<h1>Cultural Heritage</h1>
+								</div>
+								
+							</div>
+							<div class="tour_after_title">
+								<div class="meta_date">
+									<!-- <span>7 DAYS 6 NIGHTS</span> -->
+								</div>
+								<!-- <div class="meta_values">
+									<span>Category:</span>
+									<div class="value">
+										<a href="tours.html" rel="tag">Escorted Tour</a>,
+										<a href="tours.html" rel="tag">Rail Tour</a>
+									</div>
+								</div> -->
+								<div class="tour-share">
+									<ul class="share-social">
+										<li>
+											<a target="_blank" class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+										</li>
+										<li>
+											<a target="_blank" class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+										</li>
+										<li>
+											<a target="_blank" class="pinterest" href="#"><i class="fa fa-pinterest"></i></a>
+										</li>
+										<li>
+											<a target="_blank" class="googleplus" href="#"><i class="fa fa-google"></i></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div id="slider" class="flexslider">
+								<ul class="slides">
+									<li>
+										<a href="images/Tripura/cultureheritage.jpg" class="swipebox" title=""><img width="950" height="700" src="images/Tripura/cultureheritage.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+									<li>
+										<a href="images/tour/AUSTRALIANMEMORIES.jpg" class="swipebox" title=""><img width="950" height="700" src="images/tour/AUSTRALIANMEMORIES.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+									<li>
+										<a href="images/tour/tour-3.jpg" class="swipebox" title=""><img width="950" height="700" src="images/tour/tour-3.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+									<li>
+										<a href="images/tour/Highlight.jpg" class="swipebox" title=""><img width="950" height="700" src="images/tour/Highlight.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+									<li>
+										<a href="images/tour/tour-5.jpg" class="swipebox" title=""><img width="950" height="700" src="images/tour/tour-5.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+									<li>
+										<a href="images/tour/tour-6.jpg" class="swipebox" title=""><img width="950" height="700" src="images/tour/tour-6.jpg" class="attachment-shop_single size-shop_single wp-post-image" alt="" title="" draggable="false"></a>
+									</li>
+								</ul>
+							</div>
+							<div id="carousel" class="flexslider thumbnail_product">
+								<ul class="slides">
+									<li>
+										<img width="150" height="100" src="images/Tripura/cultureheritage.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+									<li>
+										<img width="150" height="100" src="images/tour/AUSTRALIANMEMORIES.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+									<li>
+										<img width="150" height="100" src="images/tour/tour-3.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+									<li>
+										<img width="150" height="100" src="images/tour/Highlight.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+									<li>
+										<img width="150" height="100" src="images/tour/tour-5.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+									<li>
+										<img width="150" height="100" src="images/tour/tour-6.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="" title="" draggable="false">
+									</li>
+								</ul>
+							</div>
+							<div class="clear"></div>
+							<div class="single-tour-tabs wc-tabs-wrapper">
+								<ul class="tabs wc-tabs" role="tablist">
+									<li class="description_tab active" role="presentation">
+										<a href="#tab-description" role="tab" data-toggle="tab">Description</a>
+									</li>
+									<li class="itinerary_tab_tab" role="presentation">
+										<a href="#tab-itinerary_tab" role="tab" data-toggle="tab">Itinerary</a>
+									</li>
+									<li class="location_tab_tab" role="presentation">
+										<a href="#tab-location_tab" role="tab" data-toggle="tab">Location</a>
+									</li>
+									<li class="reviews_tab" role="presentation">
+										<a href="#tab-reviews" role="tab" data-toggle="tab">Reviews (1)</a>
+									</li>
+								</ul>
+								<div class="tab-content">
+									<div role="tabpanel" class="tab-pane single-tour-tabs-panel single-tour-tabs-panel--description panel entry-content wc-tab active" id="tab-description">
+										<h2>Product Description</h2>
+										<p>The State of Tripura, with a geographical area of 10,491 sq. kms, is predominantly a hilly region. </p>
+										<p>It is surrounded on all sides by deltaic basin of Bangladesh except for a small part in North-East which adjoins Cachar district of Assam and Mizoram </p>
+										<table class="tours-tabs_table">
+											<tbody>
+											<tr>
+												<td><strong>DEPARTURE/RETURN LOCATION</strong></td>
+												<td>Directs Flights From Agartala, Silchar, Guwahati, Imphal and Delhi</td>
+												<td>Connecting Flights From Agartala, Bhubaneshwar, Chennai, Hyderabad, Mumbai, Bengalore, Ahemdabad and Ranchi</td>
+											</tr>
+											<tr>
+												<td><strong>DEPARTURE TIME</strong></td>
+												<td>Please arrive at least&nbsp;2 hours before the flight.</td>
+											</tr>
+											<tr>
+												<td><strong>INCLUDED</strong></td>
+												<td>
+													<table>
+														<tbody>
+														<tr>
+															<td><i class="fa fa-check icon-tick icon-tick--on"></i>Airfare
+															</td>
+															<td><i class="fa fa-check icon-tick icon-tick--on"></i>Accommodations
+															</td>
+														</tr>
+														<tr>
+															
+															<td><i class="fa fa-check icon-tick icon-tick--on"></i>Professional guide
+															</td>
+														</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+											<tr>
+												<td><b>NOT INCLUDED</b></td>
+												<td>
+													<table>
+														<tbody>
+														<tr>
+															<td>
+																<i class="fa fa-times icon-tick icon-tick--off"></i>Entrance fees
+															</td>
+														</tr>
+														<tr>
+															<td><i class="fa fa-times icon-tick icon-tick--off"></i>Guide&nbsp;gratuity
+															</td>
+														</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+											</tbody>
+										</table>
+										<p> Tripura is a landlocked state in North East India, where the seven contiguous states – Arunachal Pradesh, Assam, Manipur, Meghalaya, Mizoram, Nagaland and Tripura – are collectively known as the Seven Sister States. </p>
+										
+									</div>
+									<!-- <div role="tabpanel" class="tab-pane single-tour-tabs-panel single-tour-tabs-panel--itinerary_tab panel entry-content wc-tab" id="tab-itinerary_tab">
+										<div class="interary-item">
+											<p><span class="icon-left">1</span></p>
+											<div class="item_content">
+												<h2><strong>Day 1: Arrive Sydney</strong></h2>
+					
+												<ul>
+													<li>Arrive into Sydney. Transfer to your hotel. Rest of the day at leisure.</li>
+												</ul>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">2</span></p>
+											<div class="item_content">
+												<h2><strong>Day 2: Sydney - City tour of Sydney </strong></h2>
+												<p>View the Opera House and Darling Harbour, Manly Beach Tour through Sydney's northern beaches. You will be dropped off at the Darling Harbour, to shop at the city center on your own </p>
+												<p>This evening enjoy Sydney Showboat dinner cruise. Enjoy the dining experience with million-dollar view of Sydney harbour and Opera house with Sydney Showboat showgirls showcasing the glamorous and gorgeous performances. </p>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">3</span></p>
+											<div class="item_content">
+												<h2><strong>Day 3: Sydney</strong></h2>
+												<ul>
+													<li>Visit Blue Mountains with Featherland Wildlife Park. Head west to the Blue Mountains, see the Three Sisters rock formation and tour the mountain town of Katoomba. </li>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">4</span></p>
+											<div class="item_content">
+												<h2><strong>Day 4 : Sydney – Brisbane – Gold Coast</strong></h2>
+												<p>Transfer to Sydney Airport for flight to Brisbane. Arrive Brisbane transfer to your hotel in Gold Coast. Rest of the day is at leisure. </p>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">5</span></p>
+											<div class="item_content">
+												<h2><strong>Day 5: Gold Coast</strong></h2>
+												<p>Today visit Movie World, the ‘Hollywood on the Gold Coast’. Enjoy heart-thumping thrill rides to the laugh-a-minute antics of the Police Academy Stunt Show. Overnight in Gold Coast.</p>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">6</span></p>
+											<div class="item_content">
+												<h2><strong>Day 6: Gold Coast</strong></h2>
+												<p>Visit Sea World, and see Australia's only Polar bears in their home at Polar Bear Shores. See the world-class skiing show, make contact with marine life wonders and experience a whole world of amazing rides, shows and attractions. Overnight in Gold Coast.</p>
+											</div>
+										</div>
+										<div class="interary-item">
+											<p><span class="icon-left">7</span></p>
+											<div class="item_content">
+												<h2><strong>Day 7: Gold Coast – Depart Brisbane </strong></h2>
+												<p>Check out and transfer to Brisbane Airport for flight back home</p>
+											</div>
+										</div>
+									</div> -->
+									<div role="tabpanel" class="tab-pane single-tour-tabs-panel single-tour-tabs-panel--location_tab panel entry-content wc-tab" id="tab-location_tab">
+										<p>Agartala, West Tripura</p>
+										<div class="wrapper-gmap">
+											<div id="googleMapCanvas" class="google-map" data-lat="50.893577" data-long="-1.393483" data-address="European Way, Southampton, United Kingdom"></div>
+										</div>
+									</div>
+									<div role="tabpanel" class="tab-pane single-tour-tabs-panel single-tour-tabs-panel--reviews panel entry-content wc-tab" id="tab-reviews">
+										<div id="reviews" class="travel_tour-Reviews">
+											<div id="comments">
+												<h2 class="travel_tour-Reviews-title">1 review for
+													<span>Cultural Heritage</span></h2>
+												<ol class="commentlist">
+													<li itemscope="" itemtype="http://schema.org/Review" class="comment byuser comment-author-physcode bypostauthor even thread-even depth-1" id="li-comment-62">
+														<div id="comment-62" class="comment_container">
+															<img alt="" src="images/avata.jpeg" class="avatar avatar-60 photo" height="60" width="60">
+															<div class="comment-text">
+																<div class="star-rating" title="Rated 4 out of 5">
+																	<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+																</div>
+																<p class="meta">
+																	<strong>physcode</strong> –
+																	<time datetime="2017-01-24T03:54:04+00:00">January 24, 2017</time>
+																	:
+																</p>
+																<div class="description">
+																	<p>Mattis interdum nunc massa. Velit. Nonummy penatibus luctus</p>
+																</div>
+															</div>
+														</div>
+													</li>
+												</ol>
+											</div>
+											<div id="review_form_wrapper">
+												<div id="review_form">
+													<div id="respond" class="comment-respond">
+														<h3 id="reply-title" class="comment-reply-title">Add a review</h3>
+														<form action="#" method="post" id="commentform" class="comment-form" novalidate="">
+															<p class="comment-notes">
+																<span id="email-notes">Your email address will not be published.</span> Required fields are marked
+																<span class="required">*</span></p>
+															<p class="comment-form-author"><label for="author">Name
+																<span class="required">*</span></label>
+																<input id="author" name="author" type="text" value="" size="30" required="">
+															</p>
+															<p class="comment-form-email"><label for="email">Email
+																<span class="required">*</span></label>
+																<input id="email" name="email" type="email" value="" size="30" required="">
+															</p>
+															<p class="comment-form-rating">
+																<label>Your Rating</label>
+															</p>
+															<p class="stars">
+																<span>
+																	<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+ 																 </span>
+															</p>
+
+															<p class="comment-form-comment">
+																<label for="comment">Your Review
+																	<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required=""></textarea>
+															</p>
+															<p class="form-submit">
+																<input name="submit" type="submit" id="submit" class="submit" value="Submit">
+															</p></form>
+													</div>
+												</div>
+											</div>
+											<div class="clear"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<?php include'includes/pic.php';?>
+						</div>
+						<?php include'includes/pic1.php';?>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<?php include'includes/footer.php';?>	
+	
+</div>
+<!--end coppyright-->
+<?php include'includes/script.php';?>
+
+</body>
+</html>
